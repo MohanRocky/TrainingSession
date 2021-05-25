8ae46063abbfcdea36f34b7369c4deaff955ed8c
connectwithdhanush <dhanushmech1996@gmail.com>
2021-05-25 21:14:21 +0530

    Dhanasekar code

--- src/test/java/org/test/TestingSample.java
+++ src/test/java/org/test/TestingSample.java
@@ -1,13 +1,24 @@
 package org.test;
 
 public class TestingSample {
-	
+
 	public void car() {
 		String n = "BMW";
 		System.out.println(n);
 	}
+
 	public void seps() {
 		String d = "Digital";
 		System.out.println(d);
+	}
+
+	public void car111111() {
+		String n1 = "Jaguar";
+		System.out.println(n1);
+	}
+
+	public void seps11111() {
+		String d1 = "Digitalindia";
+		System.out.println(d1);
 	}
 }
