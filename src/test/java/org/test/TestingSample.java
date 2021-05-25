a6f2ef73c76fefe6e9fca336a5f087fc1c311339
MohanRocky <mohanv20394@gmail.com>
2021-05-25 20:43:40 +0530

    Master Code

---
+++ src/test/java/org/test/TestingSample.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class TestingSample {
+	
+	public void car() {
+		String n = "BMW";
+		System.out.println(n);
+	}
+	public void seps() {
+		String d = "Digital";
+		System.out.println(d);
+	}
+}
