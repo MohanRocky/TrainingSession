d5172eb2e2df3acc6f2a93a6ae3f5f55d7b8c41f
Tamilarasanram <ramtamilarasan096@gmail.com>
2021-05-25 21:14:31 +0530

    updated

--- src/test/java/org/test/TestingSample.java
+++ src/test/java/org/test/TestingSample.java
@@ -10,4 +10,12 @@ public class TestingSample {
 		String d = "Digital";
 		System.out.println(d);
 	}
+	public void carType() {
+		String t="Alloy";
+		System.out.println(t);
+	}
+	public void carName() {
+		String k="Kia";
+		System.out.println(k);
+}
 }
